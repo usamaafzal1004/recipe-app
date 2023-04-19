995a773b479eb1f11e9cace0346133f31e578e18
usamaafzal1004 <126756994+usamaafzal1004@users.noreply.github.com>
2023-04-19 08:33:58 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -20,17 +20,24 @@ searchForm.addEventListener('submit', (e) => {
 });
 
 async function fetchAPI() {
-  const baseURL = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${APP_key}&query=${searchQuery}&addRecipeInformation=true&number=9`;
+  const baseURL = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${APP_key}&query=${searchQuery}&addRecipeInformation=true&number=6`;
   const response = await fetch(baseURL);
   const data = await response.json();
   generateHTML(data.results);
   console.log(data);
 }
 
-function generateHTML(results) {
+async function fetchIngredients(id) {
+  const response = await fetch(`https://api.spoonacular.com/recipes/${id}/ingredientWidget.json?apiKey=${APP_key}`);
+  const data = await response.json();
+  return data.ingredients.map(ingredient => ingredient.name);
+}
+
+async function generateHTML(results) {
   container.classList.remove('initial');
   let generatedHTML = '';
-  results.map((result) => {
+  for (const result of results) {
+    const ingredients = await fetchIngredients(result.id);
     generatedHTML += `
       <div class="item">
         <img src="${result.image}" alt="img">
@@ -38,9 +45,12 @@ function generateHTML(results) {
           <h1 class="title">${result.title}</h1>
           <a class="view-btn" target="_blank" href="${result.sourceUrl}">View Recipe</a>
         </div>
+        <ul class="item-data">
+            ${ingredients.map(ingredient => `<li>${ingredient}</li>`).join('')}
+          </ul>
       </div>
     `;
-  });
+  }
   searchResultDiv.innerHTML = generatedHTML;
 }
 
